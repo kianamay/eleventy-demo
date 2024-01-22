941e311e3c9cf202464ea9bb66119fc028b2094e
kianamay <92769721+kianamay@users.noreply.github.com>
2024-01-22 07:21:35 +0000

    moon

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,12 @@ module.exports = function(eleventyConfig) {
     // Set custom directories for input, output, includes, and data
     // Copy `src/style.css` to `_site/style.css`
   eleventyConfig.addPassthroughCopy("src/style.css");
+  
+  eleventyConfig.addPassthroughCopy("img");
+
+  // Copy `css/fonts/` to `_site/css/fonts`
+  // Keeps the same directory structure.
+  eleventyConfig.addPassthroughCopy("css/fonts");
     return {
       dir: {
         input: "src",
---
+++ src/blog/moon.md
@@ -0,0 +1,7 @@
+---
+layout: layout.njk
+title: Moon
+tags: blog
+---
+# Moon
+The moon, Earth's sole natural satellite, is a desolate and airless celestial body devoid of life-sustaining conditions. Its surface is characterized by a stark landscape marked with craters, mountains, and expansive plains. Serving as a pivotal player in Earth's tidal processes, the moon's barren beauty stands in sharp contrast to our home planet.
