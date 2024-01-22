2a2ae5e9a7c1ab1761a5ee31fe0fdfe4ab17662f
kianamay <92769721+kianamay@users.noreply.github.com>
2024-01-22 07:27:59 +0000

    passthrough img

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,9 @@ module.exports = function(eleventyConfig) {
     // Set custom directories for input, output, includes, and data
     // Copy `src/style.css` to `_site/style.css`
   eleventyConfig.addPassthroughCopy("src/style.css");
-  
-  eleventyConfig.addPassthroughCopy("img");
+   // Find and copy any `png` files in any folder to _site/img
+  // Does not keep the same directory structure.
+  eleventyConfig.addPassthroughCopy({"**/*.png": "img"});
 
   // Copy `css/fonts/` to `_site/css/fonts`
   // Keeps the same directory structure.
--- src/blog/earth.md
+++ src/blog/earth.md
@@ -7,3 +7,4 @@ tags: blog
 Earth is the third planet from the sun in our solar system, characterized by a diverse range of ecosystems, abundant water, and a life-supporting atmosphere. Its varied landscapes include oceans, mountains, forests, and deserts, making it a unique and habitable celestial body.
 
 
+
